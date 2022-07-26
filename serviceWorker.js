003a2fffcc3274ad6228e309e59ad837dae4f7aa
Vadivel_Anandh <dbxdev.anandh@prema.consulting>
2022-07-26 14:34:45 +0100

    added pwa required files

---
+++ serviceWorker.js
@@ -0,0 +1,25 @@
+const staticTodo =  "todo-app-v1";
+
+const assets= [
+    "/",
+    "/index.html",
+    "/index.css",
+    "/index.js",
+
+];
+
+self.addEventListener('install', installEvent => {
+    installEvent.waitUntil(
+        caches.open(staticTodo).then(cache=> {
+            cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener("fetch", fetchEvent =>{
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
